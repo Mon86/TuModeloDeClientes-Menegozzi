106a39db9a4599bcd3e7c1d2af9ac7a47f89523f
Monhugo86 <hugomon.1901@gmail.com>
2023-10-23 21:59:53 -0300

    final

---
+++ TuModeloDeClientes+Menegozzi/cliente.py
@@ -0,0 +1,22 @@
+class Cliente:
+    def __init__(self, nombre, apellido, correo, direccion):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.correo = correo
+        self.direccion = direccion
+        self.carrito_compras = []
+
+    def agregar_producto(self, producto):
+        self.carrito_compras.append(producto)
+    
+    def mostrar_carrito(self):
+        print(f"Carrito de {self.nombre} {self.apellido}:")
+        for producto in self.carrito_compras:
+            print(f"- {producto}")
+    
+    def realizar_compra(self):
+        total = sum(producto.precio for producto in self.carrito_compras)
+        print(f"{self.nombre} {self.apellido} ha realizado una compra por un total de ${total:.2f}")
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
